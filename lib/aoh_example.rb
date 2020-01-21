194e7710a997722c076ed43118c1b350d8bb649b
Placido Wang <placidow@gmail.com>
2020-01-21 00:32:54 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -21,6 +21,8 @@ def assembled_aoh
   array_sorted
 end
 
+assembled_aoh
+
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
